7c5af543f98108dc9595741bf06e9dceb2d9a036
Khairullina16 <81875457+Khairullina16@users.noreply.github.com>
2021-12-20 02:46:07 +0500

    Исправлены ошибки в лабораторной №3
    Исправлены ошибки в лабораторной №3

--- s3-6.php
+++ s3-6.php
@@ -1,24 +1,20 @@
-<HEAD> 
+<html>
 <body bgcolor="#FFDAB9">
-<TITLE>Хайруллина Элина</TITLE> </HEAD>
-<?
-$s3 = $_POST["var19"];
-var19($s3);
-function var19($s) {
-	$v=0;
-	$n=0;
-	for ($i=0;$i<mb_strlen($s,'utf-8');$i++){
-		$str = mb_substr($s,$i,1,'utf-8');
-		if (preg_match('~^([А-ЯЁ]|[A-Z])~u', $str)==1){
-			$v++;
-		}
-		if (preg_match('~^[а-яё]|[a-z]~u', $str)==1){
-			$n++;
-		}
-	}
-	echo ("Строчных букв:".round(($n/mb_strlen($s,'utf-8')*100),1)."%");
-	echo ("<br>Прописных букв:".round(($v/mb_strlen($s,'utf-8')*100),1)."%");
-}	
- ?>
+<TITLE>Хайруллина Элина</TITLE> 
+<FORM action="<?php print $PHP_SELF ?>" method="post">
+<p>Вариант 17</p>
+<Input type="text" name = "var17" required><br>
+ <P> <INPUT type="submit" name="res" value="Вывести">
+ </FORM>
+<?php
+if(isset($_POST["res"])){
+    if( preg_match( '~^[\d]+$~', $_POST["var17"] ) )
+        echo 1;
+    elseif( preg_match( '~^[\d]+\.[\d]+$~', $_POST["var17"] ) )
+        echo 2;
+    elseif( ( (int) $str ) == 0 )
+        echo 0;
+}
+?>
 </BODY>
 </HTML>
